dfd403653bc672d5d62737b9363d5ba67506ef8d
SARA GINSBURG <sara.ginsburg.ny@gmail.com>
2021-12-26 13:49:13 -0500

    User Registration

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
 
